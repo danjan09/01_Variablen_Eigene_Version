7a36697940909c5154572129ea238a18dc62924d
Daniel <87977097+danjan09@users.noreply.github.com>
2021-07-28 21:05:08 +0200

    Create script_01.js

---
+++ script_01.js
@@ -0,0 +1,43 @@
+/***** Variablen 01 *******/
+
+// Zeilen-Kommentar
+
+/**
+ *  Block -Kommentar
+ *  Wir nutzen JavaScript 6
+ */
+
+// Ausgabe
+/* console.log("Hallo Welt!");
+console.log(firstName); // nicht deklarierte Var. --> Fehler */
+
+// 02 Deklaration + Wertzuweisung I
+
+// let firstName;       // Deklaration (Definition)
+// firstName = "Max";       // Wertzuweisung
+// console.log(firstName);  // Ausgabe
+
+// let familyName = "Mütze";    // Deklaration + Wertzuweisung
+// console.log(familyName);     // Ausgabe
+// console.log(firstName + " " + familyName); // Ausgabe
+
+// 03 Deklaration + Wertzuweisung II
+
+// let firstName,familyName;
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Namen eingeben:");
+// console.log(firstName);      // Ausgabe
+// console.log(familyName);     // Ausgabe
+// console.log("Hallo " + firstName + " " + familyName + "!"); // Ausgabe
+
+// JS ist eine untypisierte Sprache!  |  untyped
+
+let test;       // Deklaration (Definition)
+test = "hi";    // string 
+test = 2;       // number
+test = true;    // boolean
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+
+// Test
